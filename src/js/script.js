4b7513187992776e47180f3777225c8473723aba
Arthur Souza <arthur_vitor16@outlook.com>
2017-10-20 02:35:41 -0300

    Adicionado conteúdo

--- src/js/script.js
+++ src/js/script.js
@@ -2,10 +2,11 @@ jQuery(document).ready(function() {
 
 	jQuery('#fullpage').fullpage({
     paddingTop: '40px',
-    sectionsColor: ['#ffffff', '#f18889', '#4BBFC3', '#7BAABE', 'whitesmoke', '#ccddff'],
-    anchors: ['page1', 'page2', 'page3', 'page4', 'page5'],
+    sectionsColor: ['#ffffff', '#f18889', '#65b5b5', '#7BAABE', '#f18889'],
+    anchors: ['inicio', 'o-cancer', 'exames', 'curiosidades', 'produção'],
     menu: '.header__nav__list',
-    scrollOverflow: true
+    scrollOverflow: true,
+    // slidesNavigation: true,
   });
 
   jQuery('#show-navbar').on('click', function(event) {
